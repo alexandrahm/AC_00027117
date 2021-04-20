3d9b3587d98dab7ac29eed1aa074b777a483efac
alexandrahm <00027117@uca.edu.sv>
2021-04-19 20:18:25 -0600

    actualizando

--- Tarea de laboratorio #2/lab2.asm
+++ Tarea de laboratorio #2/lab2.asm
@@ -1,26 +1,13 @@
 	org	100h
 
 	section .text
-	mov	AX, [200h] 
-	mov [200h],"A"
-	mov	byte [201h], "M"
-	mov	byte [202h], "H"
-	mov	byte [203h], "M"
 
-; Direccionamiento directo o absoluto
-	mov 	[200h], AX 
-	mov 	SI, [200h]
-
-; Direccionamiento indirecto por registro
-; Usamos el registro base como puntero para acceder a la memoria
-	mov	CX, [201h]
-
-; Direccionamiento indirecto base mas indice 
-; Usamos el registro base junto con un registro apuntador
-	mov 	DX, [202h+DI]
-	mov	[202h+DI], DX
-
-; Direccionamiento relativo por registro
-	mov	AX, [DI+203h]
-
-	int 	20h
+	mov AX, "A" 
+        mov CX, "M"
+	mov DX, "H"
+	mov DI, "M"
+	    mov byte [200h], AX
+	    mov byte [201h], CX
+	    mov byte [202h], DX
+	    mov byte [203h], DI
+	    int 20h 
